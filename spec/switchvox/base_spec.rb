8bd47c6ca4f669f1846c3a943b835796c18339a0
Rimian Perkins <r.perkins@rimian.com.au>
2012-12-01 15:22:19 +1100

    parse specs

--- spec/switchvox/base_spec.rb
+++ spec/switchvox/base_spec.rb
@@ -9,4 +9,15 @@ describe Switchvox::Base do
     req = Net::HTTP.new(host, '8080')
     Switchvox::Base.new(host ,Faker::Internet.user_name, 'password')
   end
+
+  def response_json(body)
+    Hash['response', Hash['result', body]].to_json
+  end
+
+  describe 'parse json and return object' do
+    it 'should parse json and return a string' do
+      words = Faker::Lorem.words(3)
+      switchvox.json_parse(response_json(words)).should eq words
+    end
+  end
 end
